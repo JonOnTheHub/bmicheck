bfe20414ce2a9500716183ab5eb026a6d63350c0
Jon <johnnieosaghae@gmail.com>
2026-01-21 07:04:16 +0100

    practice: translating logic into JavaScript with BMI calculator

---
+++ script.js
@@ -0,0 +1,62 @@
+// BMI Calculator Script, by Jon.
+// determines BMI and provides health advice based on the result.
+
+// START
+// ask user for weight
+// ask user for height
+// bmi = weight / (height * height)
+
+// IF bmi < 18.5
+//     situation = underweight
+// ELSE IF bmi < 25
+//     situation = healthy
+// ELSE IF bmi < 30
+//     situation = overweight
+// ELSE
+//     situation = obese
+
+// display bmi and situation
+// END
+
+
+const bmiCalc = () => {
+    let weight = parseFloat(prompt("Your weight in kilograms: "))
+    let height = parseFloat(prompt("Your height in meters: "))
+
+    let bmi = (weight / (height * height)).toFixed(1);
+    let situation
+
+    if (isNaN(weight) || isNaN(height) || height <= 0) {
+        alert("enter valid numbers. are you scared to check lmao?");
+        return;
+    }
+
+    switch (true) {
+        case (bmi < 18.5):
+            situation = "underweight. eat real food.";
+            break;
+
+        case (bmi >= 18.5 && bmi < 25):
+            situation = "a healthy weight. keep it up.";
+            break;
+
+        case (bmi >= 25 && bmi < 30):
+            situation = "overweight. eat salads.";
+            break;
+
+        case (bmi >= 30 && bmi < 35):
+            situation = "obese (Class I). eat less, walk more.";
+            break;
+
+        case (bmi >= 35 && bmi < 40):
+            situation = "obese (Class II). eat nothing, walk more.";
+            break;
+
+        default:
+            situation = "severely obese (Class III). how did you even get to this point?.";
+    }
+
+    alert(`Your BMI is ${bmi}. You are ${situation}`)
+}
+
+bmiCalc()
